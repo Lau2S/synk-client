5eb35b913807fdfeed2016b65637a1d7449f05eb
Juliana <167476145+julianab16@users.noreply.github.com>
2025-11-26 00:42:16 -0500

    Remove unused providers from useAuthStore

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { auth, googleProvider, facebookProvider } from '../lib/firebase.config';
+import { auth } from '../lib/firebase.config';
 import { 
   signInWithPopup, 
   signOut, 
